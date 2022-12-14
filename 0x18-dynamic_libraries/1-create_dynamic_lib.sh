365f505630894031294bb706a0b5bce8176b4774
salisidris229 <salisuidris229@gmail.com>
2022-12-14 05:42:35 -0800

    task

--- 0x18-dynamic_libraries/1-create_dynamic_lib.sh
+++ 0x18-dynamic_libraries/1-create_dynamic_lib.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/bash
-gcc -Wall -c -Wextra -Werror -pedantic -C -fPIC *.c 
-gcc  -shared *.o  -o liball.so *.o
+#!/bin/bash
+gcc -Wall -Wextra -Werror -pedantic -c -fPIC *.c
+gcc -shared -o liball.so *.o
 export LD_LIBRARY_PATH=.:$LD_LIBRARY_PATH
